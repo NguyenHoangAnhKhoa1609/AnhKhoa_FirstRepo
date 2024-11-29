1ee0fc6e89271c3b21d63b271aa9c5118664a314
Khoa Nguyen <khoa.nguyenhoang.cit22@eiu.edu.vn>
2024-11-29 19:24:01 +0700

    Update print-name

--- src/helo.java
+++ src/helo.java
@@ -2,6 +2,7 @@ public class helo {
     public static void main(String[] args) {
         System.out.println("YAYAYAYAYA");
         System.out.println("heloo, welcome to my repo");
+        System.out.println("welcomeee");
     }
 
 }
